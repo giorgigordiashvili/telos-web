26644c75c34c8cba268c53e12c3b00fb21d1ed52
Giorgi Gordiashvili <giorgigordiashvili@mackbooks-MacBook-Pro.local>
2025-04-11 12:28:56 +0400

    fixes #2 #3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,34 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+import StyledComponentsRegistry from '../../lib/registry'
+import './globals.css'
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+  variable: '--font-geist-sans',
+  subsets: ['latin']
+})
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+  variable: '--font-geist-mono',
+  subsets: ['latin']
+})
 
 export const metadata: Metadata = {
-  title: "TELOS LLC | Innovative Software Development",
-  description: "TELOS LLC is a Tbilisi-based IT company specializing in React, React Native, Next.js, and backend development with Node.js and Laravel. We deliver cutting-edge solutions for web and mobile applications, helping businesses scale with customized software solutions.",
-};
+  title: 'TELOS LLC | Innovative Software Development',
+  description:
+    'TELOS LLC is a Tbilisi-based IT company specializing in React, React Native, Next.js, and backend development with Node.js and Laravel. We deliver cutting-edge solutions for web and mobile applications, helping businesses scale with customized software solutions.'
+}
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
       </body>
     </html>
-  );
+  )
 }
---
+++ src/components/Typography.tsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const H1 = styled.h1`
+  font-size: 50px;
+  font-weight: bold;
+  line-height: 65px;
+`
+
+const H2 = styled.h2`
+  font-size: 36px;
+  font-weight: bold;
+  line-height: 47px;
+`
+
+const H3 = styled.h3`
+  font-weight: bold;
+  font-size: 26px;
+  line-height: 34px;
+`
+
+const H4 = styled.h4`
+  font-weight: bold;
+  font-size: 26px;
+  line-height: 34px;
+`
+
+const ParagraphBold = styled.p`
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 19px;
+`
+
+const ParagraphMedium = styled.p`
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 19px;
+`
+
+type TypographyProps = {
+  variant: 'h1' | 'h2' | 'h3' | 'h4' | 'paragraph-bold' | 'paragraph-medium'
+  children: React.ReactNode
+  className?: string
+}
+
+function Typography({ variant, children, className }: TypographyProps) {
+  switch (variant) {
+    case 'h1':
+      return <H1 className={className}>{children}</H1>
+    case 'h2':
+      return <H2 className={className}>{children}</H2>
+    case 'h3':
+      return <H3 className={className}>{children}</H3>
+    case 'h4':
+      return <H4 className={className}>{children}</H4>
+    case 'paragraph-bold':
+      return <ParagraphBold className={className}>{children}</ParagraphBold>
+    case 'paragraph-medium':
+      return <ParagraphMedium className={className}>{children}</ParagraphMedium>
+    default:
+      return null
+  }
+}
+
+export { H1, H2 }
+export default Typography
