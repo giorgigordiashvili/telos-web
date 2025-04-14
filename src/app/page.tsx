ca76cf2c14bc06acb7c031bbc53a7beabdcafd54
Maariami <mariam8bidzinashvili@gmail.com>
2025-04-14 12:21:01 +0400

    this fixes #19 Checkbox

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
 import Logo from "@/components/Logo";
 import styles from "./page.module.css";
-import PrimeryButton from "@/components/PrimeryButton";
+import PrimeryCheckbox from "@/components/PrimeryCheckbox";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <Logo />
+        <PrimeryCheckbox label="By submitting this form, you agree to our Privacy Policy"></PrimeryCheckbox>
         <p>Our website is under construction. Please check back later</p>
-        <PrimeryButton variant="blue">lurji knopka</PrimeryButton>
-        <PrimeryButton variant="white">tetri knopka</PrimeryButton>
       </main>
     </div>
   );
